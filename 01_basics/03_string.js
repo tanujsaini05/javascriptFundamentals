be0816fadd2872f95ac580eb88d5298139fda485
Tanuj <115523422+tanujsaini05@users.noreply.github.com>
2026-02-06 10:59:22 +0000

    String operations

---
+++ 01_basics/03_string.js
@@ -0,0 +1,9 @@
+let name_ = 'Tanuj'
+let repo = 19
+// console.log(`Hello my name is ${name_} and my repocount is ${repo} `);
+
+// use .replace , .substring, .charAt, .indexOf, .slice, .includes, .toUpperCase(), .length()
+
+let style = "Jagannath_mahaprabhu";
+// console.log(style.toUpperCase());
+console.log(`let perform all operation to jagannath_mahaprabhu but question is how ${style.substring(0,8)} is the substring from style strin, char at index 5 ${style.charAt(5)}, index of g ${style.indexOf("g")}, slice from string ${style.slice(2,7)}, is maha present in string ${style.includes('maha')}, is length of ${style.length}. That's all`); 
