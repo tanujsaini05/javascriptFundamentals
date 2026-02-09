8af5dc9d09d0a292434e84602c5404445e17b60c
Tanuj <115523422+tanujsaini05@users.noreply.github.com>
2026-02-09 09:16:39 +0000

    Array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,1 +1,14 @@
-console.log("let's work with nested arrays ");
+console.log("let's work with nested array");
+let arr = ["Shyam ju","Shyama ju","Lala","buggo","Mma"]
+let arr2 = ["maharani Bala sundari","Maa Amba","Maa jagdamba","Maa kali","Maha mmaya Vaishno devi ji"]
+// arr.push(arr2)
+// console.log(arr[5][4]);
+
+
+// use concat
+
+arr.concat(arr2);
+console.log(arr.concat(arr2)); // forms a new array 
+console.log(Array.from("Thakur"));
+console.log("Thakurji".split(''));
+
